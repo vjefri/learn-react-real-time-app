61dcf9942e6bf0103bfca7cab6804c99897904cc
Jefri Vanegas <vjefri@gmail.com>
2016-03-03 23:49:44 -0800

    wepback working for react app

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: {
+    main: [
+      './src/main.js'
+    ]
+  },
+  output: {
+    filename: './public/[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+};
