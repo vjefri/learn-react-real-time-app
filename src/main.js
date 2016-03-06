451cecc9c27b63fa41e142b2686d74e6e0d6a66b
Jefri Vanegas <vjefri@gmail.com>
2016-03-05 23:44:58 -0800

    (feat) more dev dependacies and linter

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import App from './components/App.jsx';
+import App from './components/app.jsx';
 
-React.render(<App />, document.getElementById('container'));  
+React.render(<App />, document.getElementById('container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: {
-    main: [
-      './src/main.js'
-    ]
-  },
-  output: {
-    filename: './public/[name].js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  }
+	// this lets you debug in the browser with source maps
+	devtool: 'eval-source-map',
+	entry: {
+		main: [
+			'./src/main.js'
+		]
+	},
+	output: {
+		filename: '[name].js',
+		path: path.join(__dirname, 'public'),
+		publicPath: '/public/'
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			include: path.join(__dirname, 'src'),
+			// this passes the code through the loaders RIGHT TO LEFT
+			loader: 'react-hot!babel'
+		}, {
+			test: /\.scss$/,
+			include: path.join(__dirname, 'src'),
+			loader: 'style!css!sass'
+		}]
+	}
 };
